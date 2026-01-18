5b27f5ebdd84917a61e42c23ad57dd52567ad9d9
Ben Wassum <90662873+Bennywass@users.noreply.github.com>
2026-01-18 12:21:54 -0700

    FIxed hardcoded import

--- src/types/want.ts
+++ src/types/want.ts
@@ -1,4 +1,4 @@
-import {Want} from '/BYUI/TradeEquilizer/src/types/index';
+import {Want} from './index';
 
 // Summary type for Want, used in lists where full details are not needed
 export interface wantSummary extends Pick<Want, 'id' | 'itemId'| 'quantity' | 'priority'> {}
